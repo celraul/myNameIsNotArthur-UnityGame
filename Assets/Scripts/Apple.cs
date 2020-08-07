559e17bb9b250493b6ba6416268d3d1e7b273efa
celraul <celraul17@gmail.com>
2020-08-07 18:44:10 -0300

    estudo prefabs

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+    public GameObject collected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+
+            Destroy(gameObject, 0.5f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+}
