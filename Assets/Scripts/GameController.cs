081df9b4be5dc217af8540c94134eeac29f3e09d
celraul <celraul17@gmail.com>
2020-08-07 19:31:50 -0300

    score

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int TotalScore;
+    public Text ScoreText;
+
+    public static GameController instance; //seguindo o tutorial
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateScoreText()
+    {
+        ScoreText.text = TotalScore.ToString();
+    }
+}
