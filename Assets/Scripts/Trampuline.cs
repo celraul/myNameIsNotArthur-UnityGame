78a1056af66aabe6d6d372c4cbf1607ec70aaa10
celraul <celraul17@gmail.com>
2020-08-08 19:10:17 -0300

    level 3

---
+++ Assets/Scripts/Trampuline.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Trampuline : MonoBehaviour
+{
+    public float JumpForce;
+    private Animator Anim;
+
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            Anim.SetTrigger("Jump");
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(
+                new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
