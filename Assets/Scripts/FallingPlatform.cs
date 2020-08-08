b1ab84398a77d97367d85ed4300f52a9d2b171b1
celraul <celraul17@gmail.com>
2020-08-08 00:13:32 -0300

    level 2

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float FallingTime;
+    private TargetJoint2D Target;
+    private BoxCollider2D BoxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GetComponent<TargetJoint2D>();
+        BoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("Falling", FallingTime);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Falling()
+    {
+        Target.enabled = false;
+        BoxCollider.isTrigger = true;
+    }
+}
